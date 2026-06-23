c12c0b75e22e979eda98ad4ae4bb9b94fcd9fdad
Ritik Kumar <ritik31dr@gmail.com>
2026-06-23 23:47:04 +0530

    add file

---
+++ src/test/java/net/engineeringdigest/journalApp/service/UserServiceTests.java
@@ -0,0 +1,13 @@
+package net.engineeringdigest.journalApp.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UserServiceTests {
+
+    @Test
+    public void testAdd() {
+        assertEquals(4, 2 + 2);
+    }
+}
