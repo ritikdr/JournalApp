e2d7c76a65e1da412de7608648d49fceab1cc9b8
Ritik Kumar <ritik31dr@gmail.com>
2026-06-24 19:57:40 +0530

    update test file

--- src/test/java/net/engineeringdigest/journalApp/service/UserServiceTests.java
+++ src/test/java/net/engineeringdigest/journalApp/service/UserServiceTests.java
@@ -1,13 +1,36 @@
 package net.engineeringdigest.journalApp.service;
 
+import net.engineeringdigest.journalApp.repository.UserRepository;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 public class UserServiceTests {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Disabled
     @Test
-    public void testAdd() {
+    public void testFindByUserName() {
         assertEquals(4, 2 + 2);
+//        assertNotNull(userRepository.findByUserName("ram"));
+        assertTrue(5 > 3);
+    }
+
+    @ParameterizedTest
+    @CsvSource ({
+        "1,1,2",
+        "2,5,7",
+        "3,5,7"
+    })
+    public void test(int a, int b, int expected) {
+        assertEquals(expected, a + b);
     }
 }
